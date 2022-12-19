6488ea8dc98e4aa0904a82f18c33289225d7ff5b
Sveta Shi <svetashiblieva@gmail.com>
2022-12-19 14:34:44 +0300

    домашнее задание, задачи по теме 'Знакомство с Python'

---
+++ Homework_Python/Homework_Знакомство/Задача_3.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+number = int(input())
+
+if number == 1:
+    print('x > 0 and y > 0')
+elif number == 2:
+    print('x < 0 and y > 0')
+elif number == 3:
+    print('x < 0 and y < 0')
+elif number == 4:
+   print('x > 0 and y < 0')
+
